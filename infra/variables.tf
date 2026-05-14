1f61f3cfbe5a07b4fe1dc4631f355466136af4ef
Tayyab2802 <tayyabkhalid024@gmail.com>
2026-05-14 17:02:40 +0100

    updated varaibles file

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,48 +1,43 @@
-# =========================
-# AWS Configuration
-# =========================
-
 variable "aws_region" {
   description = "AWS region to deploy resources into"
   type        = string
+  default     = "eu-west-2"
 }
-
-# =========================
-# Project Metadata
-# =========================
 
 variable "project_name" {
   description = "Project name used for tagging AWS resources"
   type        = string
+  default     = "fyp-secure-pipeline"
 }
-
-# =========================
-# Networking Configuration
-# =========================
 
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_1_cidr" {
   description = "CIDR block for public subnet 1"
   type        = string
+  default     = "10.0.1.0/24"
 }
 
 variable "public_subnet_2_cidr" {
   description = "CIDR block for public subnet 2"
   type        = string
+  default     = "10.0.2.0/24"
 }
 
 variable "az_1" {
   description = "Availability Zone for public subnet 1"
   type        = string
+  default     = "eu-west-2a"
 }
 
 variable "az_2" {
   description = "Availability Zone for public subnet 2"
   type        = string
+  default     = "eu-west-2b"
 }
 
 
